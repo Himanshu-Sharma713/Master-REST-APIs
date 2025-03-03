d0e67c5238aa2204ed83bfd22e7f0cd9735ef063
Himanshu-Sharma713 <himanshusb713@gmail.com>
2025-03-03 12:28:08 +0530

    add commonjs

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-// Routes
+// Routes 
 // Http methods:- GET, POST, PUT, PATCH, DELETE
 app.get("/", (req:any, res:any, next:any) => {
   res.json({message:'welcome to ebook api project'})
